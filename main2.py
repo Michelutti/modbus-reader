028b30a782aba96dabe400c2b40bf11dcd6a1d38
Michelutti <rodrigomichelutti456@hotmail.com>
2023-09-05 00:12:11 -0300

    mapping done

--- main2.py
+++ main2.py
@@ -8,12 +8,13 @@ import json
 
 # Replace 'your_file.json' with the actual path to your JSON file
 file_path = 'settings.json'
+settings_data = ''
 
 try:
     with open(file_path, 'r') as json_file:
-        data = json.load(json_file)
+        settings_data = json.load(json_file)
         # Now 'data' contains the contents of the JSON file as a Python data structure
-        print(data)
+        print(settings_data)
 except FileNotFoundError:
     print(f"The file '{file_path}' was not found.")
 except json.JSONDecodeError as e:
@@ -32,21 +33,58 @@ collection = db['readings']
 client = ModbusClient(host="192.168.15.75", port=502, unit_id=1, auto_open=True)
 
 def read_modbus_data():
-    discreteInputs = client.read_discrete_inputs(0, 1)
-    inputRegister = client.read_input_registers(0, 1)
+    discreteInputs = client.read_discrete_inputs(0, 6)
+    inputRegister = client.read_input_registers(0, 6)
+    print(discreteInputs)
+    print(inputRegister)
+    # result = [{'input': boolean, 'value': integer} for boolean, integer in zip(discreteInputs, inputRegister)]
+  
+    # if result:
+    #     print(result)
+    # else:
+    #     print("read error")
+    #     return None
 
-    result = [{'input': boolean, 'value': integer} for boolean, integer in zip(discreteInputs, inputRegister)]
-
-    if result:
-        print(result)
-    else:
-        print("read error")
-        return None
-    return result
+    return generate_results(settings_data, inputRegister, discreteInputs)
 
 def store_data_in_mongodb(data):
     if data:
-        collection.insert_one({'data': data, 'timestamp': time.time()})
+        collection.insert_many(data)
+
+def generate_results(data, inputRegister, discreteInput):
+    results = []
+    timestamp = time.time()
+
+    while inputRegister and discreteInput:
+        machine_added = False
+
+        for item in data:
+            machine = item['machine']
+            map_data = item['map']
+
+            if len(inputRegister) >= machine['inputRegister'] and len(discreteInput) >= machine['discreteInput']:
+                result_object = {
+                    'machine': len(results) +1,
+                    'timestamp': timestamp
+                }
+
+                for mapping in map_data:
+                    data_type = mapping['tipo']
+                    address = mapping['endereco']
+
+                    if data_type == 'discreteInput':
+                        result_object[mapping['descricao']] = discreteInput.pop(0)
+                    elif data_type == 'inputRegister':
+                        result_object[mapping['descricao']] = inputRegister.pop(0)
+
+                results.append(result_object)
+                machine_added = True
+
+        if not machine_added:
+            # If no machine was added in this iteration, exit the loop
+            break
+
+    return results
 
 while True:
     # Read Modbus data and store it in MongoDB
